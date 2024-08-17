bf33c6d1ffe12bcb819250d57d0239699869b926
Ryan-N14 <141654254+Ryan-N14@users.noreply.github.com>
2024-08-16 23:29:32 -0500

    RewardConverter

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+
+    private double milesToCashRate = 0.0035;
+    private double cashBalance = 0;
+    private double milesBalance = 0;
+
+
+
+    public RewardValue(double cash){
+        cashBalance = cash;
+    }
+
+    public RewardValue(double miles, boolean isMiles){
+        milesBalance = miles;
+    }
+
+
+
+
+    //Methods
+    public double getCashValue(){
+        cashBalance = milesBalance * milesToCashRate; //converting the miles to cash
+        return cashBalance;
+    }
+
+
+    public double getMilesValue(){
+        milesBalance = cashBalance / milesToCashRate; //converting cash to miles
+        return milesBalance;
+    }
+
+
 }
